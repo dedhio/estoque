9001168c92dac2fc8636368c2beaabba6580cad2
Geovani Lopes <dedhio@gmail.com>
2018-04-15 18:49:09 +0000

    Listagem de produtos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -16,16 +16,11 @@ class ProdutoController extends Controller
 
     public function lista(){
 
-        $html = '<h1>Listagem de produtos com Laravel</h1>';
-        $html .= '<ul>';
         $produtos= DB::select('select * from produtos');
 
-        foreach ($produtos as $p){
-            $html .= '<li> Nome: '.$p->nome.', Descrição: '.$p->descricao.'</li>';
+        if (view()->exists('listagem'))
+        {
+            return view('listagem')->withProdutos($produtos);
         }
-
-        $html .= '</ul>';
-
-        return $html;
     }
 }
